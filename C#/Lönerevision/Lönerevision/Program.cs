8a11b2e0ebfd5049c6d73ed6476b938780f09700
Martin-Carlsson85 <martin@webfixit.se>
2022-06-01 09:59:02 +0200

    Added comments to structure the laboration

---
+++ C#/Lönerevision/Lönerevision/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lönerevision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint numberOfSaleries;
+
+            //Antal löner att mata in
+
+            //Ange lön
+
+            //Skriva ut medianlön
+
+            //Medellön
+
+            //Lönespridning
+
+            //Skriv ut lönerna, tre per rad
+
+            //Två metoder
+
+            //ReadInt() = användaren matar in antal löner
+            //Felmeddelande om färre än 2
+            //Felmeddelande om det inte är en uint
+            //Anropa ProcessSalaries() med antal löner som argument
+
+            //ProcessSalaries() = läs in antalet löner -> beräkna median, medel och lönespridning -> skriv ut lönerna under
+        }
+    }
+}
