60ed91b72f844e5311aa99962a966ababde8ff48
Martin-Carlsson85 <martin@webfixit.se>
2022-09-15 19:19:49 +0200

    Starting Pig-Game

---
+++ JavaScript/Pig-Game/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//Välja element på två olika sätt
+const score0El = document.querySelector(`#score--0`);
+const score1El = document.getElementById(`score--1`);
+const diceEl = document.querySelector(`.dice`);
+
+//Startvillkor
+score0El.textContent = 0; //JavaScript konverterar siffror till strängar här
+score1El.textContent = 0;
+devicePixelRatio.classList.add(`hidden`);
