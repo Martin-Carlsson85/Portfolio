99722c62da91692a55711bb168e9583e3847922a
Martin-Carlsson85 <martin@webfixit.se>
2022-09-17 13:49:22 +0200

    Holding current score

--- JavaScript/Pig-Game/script.js
+++ JavaScript/Pig-Game/script.js
@@ -19,29 +19,61 @@ diceEl.classList.add(`hidden`);
 const scores = [0, 0]; //Poängen som visas under Player
 let currentScore = 0; //Kan inte vara inuti funktionen då det skulle generera 0 poäng varje gång man klickar på musen
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle(`player--active`);
+  player1El.classList.toggle(`player--active`);
+};
 
 //Funktion för att kasta tärningen
 btnRoll.addEventListener(`click`, function () {
-  //Function handler
-  // 1.Här genereras tärningskastet
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    //Function handler
+    // 1.Här genereras tärningskastet
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  // 2. Display dice
-  diceEl.classList.remove(`hidden`);
-  diceEl.src = `dice-${dice}.png`;
+    // 2. Display dice
+    diceEl.classList.remove(`hidden`);
+    diceEl.src = `dice-${dice}.png`;
 
-  // 3. Kolla om tärningen är lika med noll
-  if (dice !== 1) {
-    //Lägg till tärnings värde till befintligt värde(current score)
-    currentScore = currentScore + dice; //Går även att skriva currentScore += dice
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    //SKifta till andra spelaren
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
-    player0El.classList.toggle(`player--active`);
-    player1El.classList.toggle(`player--active`);
+    // 3. Kolla om tärningen är lika med noll
+    if (dice !== 1) {
+      //Lägg till tärnings värde till befintligt värde(current score)
+      currentScore = currentScore + dice; //Går även att skriva currentScore += dice
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      //SKifta till andra spelaren
+      switchPlayer();
+    }
   }
+});
+
+btnHold.addEventListener(`click`, function () {
+  if (playing) {
+    // 1. Lägg till aktuell poäng till aktiv spelares poäng
+    scores[activePlayer] += currentScore;
+    //scores[1] = scores[1] + currentScore
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 2. Kolla om spelarens poäng är >= 100
+    if (scores[activePlayer] >= 20) {
+      // Spelet klart
+      playing = false;
+      diceEl.classList.add(`hidden`);
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove(`player--winner`);
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove(`player--active`);
+    } else {
+      switchPlayer();
+    }
+  } //Skifta till den andra spelaren
 });
